76a86fe790b1fba90660bf80a9e7b7169dd34509
Дмитро Трубіцький <trubickijd@gmail.com>
2026-05-07 16:36:17 +0300

    Додано методи піднесення до степеня та обчислювання квадратного кореня

--- maic.cpp
+++ maic.cpp
@@ -5,10 +5,27 @@
 using namespace std;
 
 class Calculator {
-
+private:
+    // --- Зона відповідальності Учасника 3 ---
+    double power(double base, double exponent) {
+        return pow(base, exponent);
+    }
+    double squareRoot(double number) {
+        if (number < 0) {
+            throw invalid_argument("Помилка: Корінь з від'ємного числа!");
+        }
+        return sqrt(number);
+    }
 };
 
 int main() {
-  
-  return 0;
+    // Тести Учасника 3
+    cout << "2 ^ 3 = " << calc.power(2, 3) << endl;
+    try {
+        cout << "Корінь з 16 = " << calc.squareRoot(16) << endl;
+    }
+    catch (const invalid_argument& e) {
+        cout << e.what() << endl;
+    }
+    return 0;
 }
