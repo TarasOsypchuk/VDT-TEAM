29f72235e5096548d74da983947f28a49edb992c
vlad-padurian <vladpadyrian2@gmail.com>
2026-05-07 16:35:46 +0300

    Додав методи multiply і divide та тести в мейні

--- maic.cpp
+++ maic.cpp
@@ -5,10 +5,24 @@
 using namespace std;
 
 class Calculator {
-
+public:
+  double multiply(double a, double b) {
+        return a * b;
+    }
+    double divide(double a, double b) {
+        if (b == 0) {
+            throw invalid_argument("Помилка: Ділення на нуль!");
+        }
+        return a / b;
+    }
 };
 
 int main() {
-  
+  cout << "6 * 7 = " << calc.multiply(6, 7) << endl;
+    try {
+        cout << "20 / 4 = " << calc.divide(20, 4) << endl;
+    } catch (const invalid_argument& e) {
+        cout << e.what() << endl;
+    }
   return 0;
 }
